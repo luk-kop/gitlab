c1f971d77c4efddf2d391ddb582e67c10c09dcf2
luk-kop <luk.kopicki@gmail>
2023-02-08 20:55:28 +0100

    func type

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import gitlab
 from dotenv import load_dotenv
 
 
-def get_gitlab(private_token: str, url: str = ''):
+def get_gitlab(private_token: str, url: str = '') -> gitlab.client.Gitlab:
     if url:
         return gitlab.Gitlab(url=url, private_token=private_token)
     return gitlab.Gitlab(private_token=private_token)
@@ -15,6 +15,7 @@ if __name__ == '__main__':
 
     GL_PRIVATE_TOKEN = os.environ.get('GL_PRIVATE_TOKEN')
     gl = get_gitlab(private_token=GL_PRIVATE_TOKEN)
+
     projects = gl.projects.list(owned=True)
     for project in projects:
         print(f'name: {project.name}, path: {project.path_with_namespace}, url: {project.web_url}')
